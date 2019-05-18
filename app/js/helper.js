9440cba109957fd3454d611ec423f0513c25359d
Stanley <Stanleyliu15@gmail.com>
2019-05-18 15:42:21 -0700

    add data points filter to map

--- app/js/helper.js
+++ app/js/helper.js
@@ -14,9 +14,39 @@ function parseDataPoints(data) {
     dataPoints.push([
       parseFloat(d.Latitude),
       parseFloat(d.Longitude),
-      parseFloat(d.receptivity)
+      parseFloat(d.receptivity) // || 0.0
     ]);
   });
 
   return dataPoints;
+}
+
+function createPoints(geoJson, color) {
+  var geojsonMarkerOptions = {
+    radius: 2,
+    weight: 0,
+    fillColor: '#DB162F',
+    fillOpacity: 0.8
+  };
+
+  return L.geoJSON(geoJson, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  });
+}
+
+function createGeoJson(data) {
+  var geojson_list = [];
+
+  data.forEach(function(d) {
+    geojson = {
+      type: 'Point',
+      coordinates: [d[0], d[1]]
+    };
+
+    geojson_list.push(geojson);
+  });
+
+  return geojson_list;
 }
--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,14 @@
 function renderMap(dataPoints) {
   var map = L.map('map', {
-    center: [dataPoints[0][0], dataPoints[0][1]],
-    zoom: 10
+    center: [39.8283, -98.5795],
+    zoom: 5,
+    minZoom: 4,
+    maxZoom: 18
   });
 
   L.tileLayer(
     'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3RhbmxleWxpdTE1IiwiYSI6ImNqdnRwaXJzMTE2ZXY0OG54bHU4cGk2bGgifQ.WuacZYwxwAMIWvxjZL9BOw',
     {
-      maxZoom: 25,
       id: 'mapbox.streets',
       accessToken:
         'pk.eyJ1Ijoic3RhbmxleWxpdTE1IiwiYSI6ImNqdnRwaXJzMTE2ZXY0OG54bHU4cGk2bGgifQ.WuacZYwxwAMIWvxjZL9BOw',
@@ -16,11 +17,24 @@ function renderMap(dataPoints) {
     }
   ).addTo(map);
 
-  L.heatLayer(dataPoints, {}).addTo(map);
+  L.heatLayer(dataPoints, {
+    // radius: 15,
+    // blur: 5,
+    // gradient: { 0: 'red', 0.33: 'red', 0.5: 'red', 0.66: 'red', 1: 'red' }
+  }).addTo(map);
 
   L.Control.geocoder().addTo(map);
   new L.Control.Geocoder.Nominatim();
   L.Control.Geocoder.nominatim();
+
+  var geojson_list = createGeoJson(dataPoints);
+  var points = createPoints(geojson_list);
+
+  var points_layer = {
+    'Display Points': points
+  };
+
+  L.control.layers({}, points_layer, { collapsed: false }).addTo(map);
 }
 
 asyncParseCSV(renderMap);

211f8842a7afd18b1718d87de472b0dabe14c67d
Stanley <Stanleyliu15@gmail.com>
2019-05-18 11:07:19 -0700

    add search functionality to map

--- mysite/mysite/static/js/main.js
+++ mysite/mysite/static/js/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([41.850033, -87.6500523], 4);
 
 L.tileLayer(
   'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3RhbmxleWxpdTE1IiwiYSI6ImNqdnRwaXJzMTE2ZXY0OG54bHU4cGk2bGgifQ.WuacZYwxwAMIWvxjZL9BOw',
@@ -11,3 +11,7 @@ L.tileLayer(
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
   }
 ).addTo(map);
+
+L.Control.geocoder().addTo(map);
+new L.Control.Geocoder.Nominatim();
+L.Control.Geocoder.nominatim();

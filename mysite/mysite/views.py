d07ae9001764ea8e8b3ad5122e0d6c8aa6653be6
Stanley <Stanleyliu15@gmail.com>
2019-05-18 17:50:54 -0700

    delete django

--- mysite/mysite/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    """
-    Loads the home page for the website.
-    :param request:
-    :return: The index.html page.
-    """
-    return render(request, 'index.html')
